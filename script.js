dfa1faa39ad89fd8ac98cb459280a1724ccc37f9
kperry42 <kperry42@uncc.edu>
2019-12-09 13:01:44 -0500

    update css and add player type and difficulty lists;

---
+++ script.js
@@ -0,0 +1,21 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function myFunction()
+{
+    var x = document.getElementById("myDiv");
+    if(x.style.display == "none")
+    {
+        x.style.display= "block";
+    }
+    else{
+        x.innerHTML= "Hello, Kayla";
+    }
+}
+
+
+
+
